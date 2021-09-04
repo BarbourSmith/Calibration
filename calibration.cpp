1551d0ea0312a99c04a04d25781119d737ca6d47
BarbourSmith <BarbourSmith@users.noreply.github.com>
2021-09-03 19:31:07 -0700

    Optimized and converging on the correct answer

--- calibration.cpp
+++ calibration.cpp
@@ -1,16 +1,19 @@
 #include "calibration.h";
 #include <Arduino.h>;
 
+// int printToWeb (int (*f) (const int arg1, const int arg2), const int a, const int b)
+// {
+
 void printIndividual(double individual[]){
     Serial.print("| ");
-    for(int i = 0; i< 9; i++){
+    for(int i = 0; i< 6; i++){
         Serial.print(individual[i]);
         Serial.print(" | ");
     }
     Serial.print("\n");
 }
 
-void printPopulation(double population[][9]){
+void printPopulation(double population[][6]){
     
     Serial.println("Population: ");
     for(int i = 0; i < 50; i++){
@@ -21,18 +24,16 @@ void printPopulation(double population[][9]){
 }
 
 //Assigns an individual to the population
-void assign(int index, double population[][9], double individual[]){
-    for(int i = 0; i< 9; i++){
+void assign(int index, double population[][6], double individual[]){
+    for(int i = 0; i< 6; i++){
         population[index][i] = individual[i];
     }
 }
 
 //Mutates an individual within the population
-void mutate(int index, double population[][9],double maxMutation){
-    for(int i = 0; i< 9; i++){
-        if(i != 4 && i != 5){ //Skip the (0,0) point that doesn't move
-            population[index][i] = population[index][i] + (random(-100.0*maxMutation,100*maxMutation)/100.0);
-        }
+void mutate(int index, double population[][6],double maxMutation){
+    for(int i = 0; i< 5; i++){
+        population[index][i] = population[index][i] + (random(-100.0*maxMutation,100*maxMutation)/100.0);
     }
 }
 
@@ -63,10 +64,10 @@ double distPointToArcs(double x,double y,double measurement[],double individual[
     double trDist = findDistanceToArc(x, y, measurement[1], individual[2], individual[3]);
     
     //Compute dist to bl arc
-    double blDist = findDistanceToArc(x, y, measurement[2], individual[4], individual[5]);
+    double blDist = findDistanceToArc(x, y, measurement[2], 0, 0);
     
     //Compute dist to br arc
-    double brDist = findDistanceToArc(x, y, measurement[3], individual[6], individual[7]);
+    double brDist = findDistanceToArc(x, y, measurement[3], individual[4], 0);
     
     //Return the average
     return (tlDist + trDist + blDist + brDist)/4.0;
@@ -82,8 +83,9 @@ void evaluateDistandMin(double currentBest[], double x,double y,double measureme
 }
 
 //Walks the gradient recursively to find the closest point
-void walkClosenessGradient(double x, double y, double stepSize, double measurement[], double individual[], double finalPoint[]){
+void walkClosenessGradient(double x, double y, double stepSize, double measurement[], double individual[], double finalPoint[], int recursionLevel){
     
+    recursionLevel = recursionLevel + 1;
     double closestPoint[] = {0,0,10000};
     
     //0,0
@@ -111,8 +113,13 @@ void walkClosenessGradient(double x, double y, double stepSize, double measureme
         finalPoint[1] = y;
         finalPoint[2] = closestPoint[2];
     }
+    else if(recursionLevel > 15){ //Don't allow deeper recursion to crash the stack
+        finalPoint[0] = x;
+        finalPoint[1] = y;
+        finalPoint[2] = closestPoint[2];
+    }
     else{  //Recursively continue walking the gradient
-        walkClosenessGradient(closestPoint[0], closestPoint[1], stepSize, measurement, individual, finalPoint);
+        walkClosenessGradient(closestPoint[0], closestPoint[1], stepSize, measurement, individual, finalPoint, recursionLevel);
     }
 }
 
@@ -122,32 +129,34 @@ double findPointClosestToArcsDist(double measurement[], double individual[]){
     double currentClosestPoint[3];
     
     //Use the middle as the initial guess
-    walkClosenessGradient(individual[2]/2,individual[3]/2, 100, measurement, individual, currentClosestPoint);
+    walkClosenessGradient(individual[2]/2,individual[3]/2, 100, measurement, individual, currentClosestPoint, 0);
     
     //Second pass
-    walkClosenessGradient(currentClosestPoint[0],currentClosestPoint[1], 10, measurement, individual, currentClosestPoint);
+    walkClosenessGradient(currentClosestPoint[0],currentClosestPoint[1], 10, measurement, individual, currentClosestPoint, 0);
     
     //Third pass
-    walkClosenessGradient(currentClosestPoint[0],currentClosestPoint[1], 1, measurement, individual, currentClosestPoint);
+    walkClosenessGradient(currentClosestPoint[0],currentClosestPoint[1], 1, measurement, individual, currentClosestPoint, 0);
     
     //Fourth pass
-    walkClosenessGradient(currentClosestPoint[0],currentClosestPoint[1], .1, measurement, individual, currentClosestPoint);
+    walkClosenessGradient(currentClosestPoint[0],currentClosestPoint[1], .1, measurement, individual, currentClosestPoint, 0);
     
-    return currentClosestPoint[2];
+    return myAbs(currentClosestPoint[2]);
 }
 
 //Evaluate the fitness of an individual
 void evaluateFitness(double individual[], double measurements[][4]){
     
     double m1Fitness = findPointClosestToArcsDist(measurements[0], individual);
-    
     double m2Fitness = findPointClosestToArcsDist(measurements[1], individual);
-    
     double m3Fitness = findPointClosestToArcsDist(measurements[2], individual);
-    
     double m4Fitness = findPointClosestToArcsDist(measurements[3], individual);
+    double m5Fitness = findPointClosestToArcsDist(measurements[4], individual);
+    double m6Fitness = findPointClosestToArcsDist(measurements[5], individual);
+    double m7Fitness = findPointClosestToArcsDist(measurements[6], individual);
+    double m8Fitness = findPointClosestToArcsDist(measurements[7], individual);
+    double m9Fitness = findPointClosestToArcsDist(measurements[8], individual);
     
-    individual[8] = (m1Fitness + m2Fitness + m3Fitness + m4Fitness)/4.0;
+    individual[5] = (m1Fitness + m2Fitness + m3Fitness + m4Fitness + m5Fitness + m6Fitness + m7Fitness + m8Fitness + m9Fitness)/9.0;
 }
 
 int sort(const void *pa, const void *pb){
@@ -156,19 +165,19 @@ int sort(const void *pa, const void *pb){
     double* ia = (double*)pa;
     double* ib = (double*)pb;
     
-    double a = myAbs(ia[8]);
-    double b = myAbs(ib[8]);
+    double a = myAbs(ia[5]);
+    double b = myAbs(ib[5]);
     
     // The comparison
     return a > b ? 1 : (a < b ? -1 : 0);
     
 }
 
-void sortPopulation(double population[][9]){
+void sortPopulation(double population[][6]){
     qsort(population, 50, sizeof(population[0]), sort);
 }
 
-void cullAndBreed(double population[][9],double stepSize){
+void cullAndBreed(double population[][6],double stepSize){
     //Population is sorted at this point
     
     for(int i = 9; i < 50; i++){
@@ -177,37 +186,68 @@ void cullAndBreed(double population[][9],double stepSize){
     }
 }
 
-void evolve(double population[][9],double stepSize,double targetFitness, int timeout){
+void evolve(double population[][6], double measurements[][4], double stepSize, double targetFitness, int timeout, void (*webPrint) (double arg1)){
     int i = 0;
-    while(population[0][8] > targetFitness){
+    double lastVal = 0;
+    int numberOfRepitions = 0; //The number of times the same value has been found
+    while(population[0][5] > targetFitness){
         
         //Sort the population
         sortPopulation(population);
         
+        if(population[0][5] == lastVal){
+            numberOfRepitions++;
+        }
+        else{
+            numberOfRepitions = 0;
+        }
+        lastVal = population[0][5];
+        if(numberOfRepitions > 25){
+            Serial.println("Stuck");
+            break;
+        }
+        
+        Serial.print("Fittest: ");
+        Serial.print(population[0][5]);
+        Serial.print("      gen: ");
+        Serial.print(i);
+        Serial.print("    step: ");
+        Serial.print(stepSize);
+        Serial.print("    reps: ");
+        Serial.println(numberOfRepitions);
+        
+        
+        webPrint(population[0][5]);
+        
         //Breed the best (Start with just mutating them)
         cullAndBreed(population, stepSize);
+        
+        //Evaluate the fitness of the new individuals
+        for(int i = 0; i < 50; i++){
+            evaluateFitness(population[i], measurements);
+        }
         
         //Repeat until the fitness function is within some threshold or timeout
         
         i++;
         if(i > timeout){
-            Serial.println("Breaking");
+            Serial.println("Timeout");
             break;
         }
     }
 }
 
 //Compute the calibration
-void computeCalibration(double measurements[][4]){
-    Serial.println("Begining to compute calibration");
+void computeCalibration(double measurements[][4], double result[6], void (*webPrint) (double arg1)){
+    Serial.println("Beginning to compute calibration");
     double initialWidth = 3000;
     double initialHeight = 1800;
     
     // Establish initial guesses for the corners
-    double initialIndividual[] = {0, initialHeight, initialWidth, initialHeight, 0, 0, initialWidth, 0, 10000};
+    double initialIndividual[] = {0, initialHeight, initialWidth, initialHeight, initialWidth, 10000};
     
     // Build a population
-    double population[50][9];
+    double population[50][6];
     for(int i = 0; i < 50; i++){
         assign(i, population, initialIndividual);
     }
@@ -224,15 +264,25 @@ void computeCalibration(double measurements[][4]){
     }
     
     //Evolve the population 
-    evolve(population, .5, .25, 2000);
-    evolve(population, .1, .01, 2000);
+    evolve(population, measurements,  5, .25, 2000, webPrint);
+    evolve(population, measurements, .5, .25, 2000, webPrint);
+    evolve(population, measurements, .1, .01, 2000, webPrint);
+    evolve(population, measurements, .01, .01, 2000, webPrint);
     
     sortPopulation(population);
     
     Serial.println("Final fittest: ");
-    Serial.println(population[0][8]);
+    Serial.println(population[0][5]);
+    
+    //Pass back the result
+    result[0] = population[0][0];
+    result[1] = population[0][1];
+    result[2] = population[0][2];
+    result[3] = population[0][3];
+    result[4] = population[0][4];
+    result[5] = population[0][5];
 
-    if(population[0][8] < .2){
+    if(population[0][5] < .2){
         Serial.println("Calibration success");
     }
     else{
--- calibration.h
+++ calibration.h
@@ -1,8 +1,8 @@
 
 void printIndividual(double individual[]);
-void printPopulation(double population[][9]);
-void assign(int index, double population[][9], double individual[]);
-void mutate(int index, double population[][9],double maxMutation);
+void printPopulation(double population[][6]);
+void assign(int index, double population[][6], double individual[]);
+void mutate(int index, double population[][6],double maxMutation);
 double findPointClosestToArcsDist(double measurement[], double individual[]);
 void evaluateFitness(double individual[], double measurements[][4]);
-void computeCalibration(double measurements[][4]);
+void computeCalibration(double measurements[][4], double result[6], void (*webPrint) (double arg1));
